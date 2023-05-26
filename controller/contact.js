a34d3481b9ee6bd85537fee907f15c08de9970e1
Yashraj Dawkhar <dawkharyashraj@gmail.com>
2023-05-26 12:01:31 +0530

    update

--- controller/contact.js
+++ controller/contact.js
@@ -6,10 +6,20 @@ const db = require('../models/contact');
 
 
 exports.save = async function (req, res) {
-
     const data = new db(req.body);
     const save = await data.save();
     res.status(201).json({ message: "Contact details have been recieved sucessfully" })
+}
 
+exports.getContactDetails = async (req, res) => {
+
+    try {
+        
+        let data = await db.find();
+        res.status(200).json(data);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 
 }
--- router/contact.js
+++ router/contact.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const d = require('../controller/contact')
 
 router.post('/', d.save)
-
+router.get('/', d.getContactDetails)
 
 module.exports = router
